10821e0916116addfedde8f06fe10365cf353b71
Muhammad Dika <92399074+muhammaddika03@users.noreply.github.com>
2021-10-13 03:21:34 +0700

    Create char.cpp

---
+++ 3. Jenis Tipe Data/char.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char a='q';
+    cout << a << endl;
+    cout << sizeof(a) << " byte" << endl; //untuk menampilkan besar tipe data //aslinya 1-bit tetapi memori tidak mengenal istilah bit istilah yang digunakan adalah byte
+    cin.get();
+    return 0;
+}
