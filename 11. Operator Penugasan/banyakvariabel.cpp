109106725eef3405b7cd40380a51b23a3a5a0c22
Muhammad Dika <92399074+muhammaddika03@users.noreply.github.com>
2021-10-13 03:35:42 +0700

    Add files via upload

---
+++ 11. Operator Penugasan/banyakvariabel.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 12, y = 31, a =21, b =52;
+    //banyak variabel
+     x += 10;
+     y += 19;
+     a /= 7;
+     b /= 13;
+
+     cout << "Nilai x akhir: " << x << endl;
+     cout << "Nilai y akhir: " << y << endl;
+     cout << "Nilai a akhir: " << a << endl;
+     cout << "Nilai b akhir: " << b << endl;
+
+     return 0;
+}
