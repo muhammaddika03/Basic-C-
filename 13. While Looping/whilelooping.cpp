b18f26f9ce02327c5cdb9672681f34c2b8188ed6
Muhammad Dika <92399074+muhammaddika03@users.noreply.github.com>
2021-10-13 03:40:05 +0700

    Create whilelooping.cpp

---
+++ 13. While Looping/whilelooping.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    //do while looping
+    int a = 15;
+
+    while (true)
+    {
+        cout <<"Hallo Dunia" << a << endl;
+    }
+    cout <<"Hallo Dunia akhir" << endl;
+    return 0;
+}
+
