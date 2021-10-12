87f92087e451442186933816f2a62395b99f4dea
Muhammad Dika <92399074+muhammaddika03@users.noreply.github.com>
2021-10-13 03:48:00 +0700

    Add files via upload

---
+++ 20. Latihan Rumus Bangun dengan fungsi return/luaspermukaandanvolumebalok.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int volume (int p, int l, int t)
+{
+    int v;
+    v = p * l * t;
+    return v;
+}
+
+int luas_permukaan (int p, int l, int t)
+{
+    int lp;
+    lp = (2 * p * l) + (2 * p * t) + (2 * l * t);
+    return lp; 
+}
+
+void hasil ()
+{}
+
+int main ()
+{
+    int hasilvolume, hasilluaspermukaan, panjang, lebar, tinggi;
+
+    cout << "Masukkan nilai panjang : ";
+    cin >> panjang;
+    cout << "Masukkan nilai lebar : ";
+    cin >> lebar;
+    cout << "Masukkan nilai tinggi : ";
+    cin >> tinggi;
+
+    hasilvolume = volume (panjang, lebar, tinggi);
+    cout << "Hasil volume balok : " << hasilvolume << endl;
+
+    hasilluaspermukaan = luas_permukaan (panjang, lebar, tinggi);
+    cout << "Hasil luas permukaan balok : " << hasilluaspermukaan << endl;
+
+    return 0;
+}
+
