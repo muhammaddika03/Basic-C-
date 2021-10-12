5ac11c4652c8b79a01d129d61c1610f4b6e2fc95
Muhammad Dika <92399074+muhammaddika03@users.noreply.github.com>
2021-10-13 03:18:55 +0700

    Create contoh penggunaan variabel program sederhana.cpp

---
+++ 2. Variabel/contoh penggunaan variabel program sederhana.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string nama;
+    int umur;
+    string gender;
+
+    cout << "Masukkan nama: ";
+    cin >> nama;
+    cout << "Masukkan umur: ";
+    cin >> umur;
+    cout << "Masukkan jenis kelamin: ";
+    cin >> gender;
+
+    cout<< "nama: "<<nama<<endl;
+    cout << "umur: "<<umur<<endl;
+    cout << "gender: "<<gender<<endl;
+    return 0;
+}
