92ab9b4ad4f7a8fb3f56c28fbec770cb2a39e26f
Muhammad Dika <92399074+muhammaddika03@users.noreply.github.com>
2021-10-16 20:19:06 +0700

    Add files via upload

---
+++ 30. Function using Reference/denganfungsiprototype.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+
+void pointer(int &);//fungsi prototype dengan reference
+//kuadrat
+void pointerkuadrat(int &);
+
+
+int main (){
+    int x = 20;
+    cout << "alamat dari x : " << &x << endl;
+    cout << "nilai dari x : " << x << endl;
+    pointer (x); 
+    pointerkuadrat(x);
+    return 0;
+}
+
+void pointer(int &b){
+    cout << "alamat dari b : " << &b << endl;
+    cout << "nilai dari b : " << b << endl; 
+}
+//kuadrat
+void pointerkuadrat(int &a){
+    a = a * a;
+    cout << "alamat dari a : " << &a << endl;
+    cout << "nilai dari a : " << a << endl; 
+}
